88d569e72b4866c68bc7ce2f8b33fed1695c99c9
overfox-company <overfoxcompany@gmail.com>
2025-04-08 19:31:52 -0400

    Update baseUrl in Docusaurus config for correct deployment path

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://Overfox-Company.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs-api-onreservy',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
